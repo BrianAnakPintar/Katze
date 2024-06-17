6ba9b8378bca8ddd745d3b09bfa7ce7015e8922b
Brian <brianfransis.moniaga@gmail.com>
2024-06-16 21:15:47 -0600

    Completed instructions

--- internal/cpu/cpu.go
+++ internal/cpu/cpu.go
@@ -1,55 +1,181 @@
 package cpu
 
-import "fmt"
-
 /*
 Flags are based on https://www.nesdev.org/wiki/Status_flags
 Notice that bit-5 is not a flag.
 */
 
 const (
-    FLAG_CARRY      uint8 = 1 << 0  // 0000 0001
-    FLAG_ZERO       uint8 = 1 << 1  // 0000 0010
-    FLAG_INTERRUPT  uint8 = 1 << 2  // 0000 0100
-    FLAG_DECIMAL    uint8 = 1 << 3  // 0000 1000
-    FLAG_BREAK      uint8 = 1 << 4  // 0001 0000
-    FLAG_OVERFLOW   uint8 = 1 << 6  // 0100 0000 
-    FLAG_NEGATIVE   uint8 = 1 << 7  // 1000 0000
+    FLAG_CARRY      byte = 1 << 0  // 0000 0001
+    FLAG_ZERO       byte = 1 << 1  // 0000 0010
+    FLAG_INTERRUPT  byte = 1 << 2  // 0000 0100
+    FLAG_DECIMAL    byte = 1 << 3  // 0000 1000
+    FLAG_BREAK      byte = 1 << 4  // 0001 0000
+    FLAG_BREAK2     byte = 1 << 5  // 0010 0000
+    FLAG_OVERFLOW   byte = 1 << 6  // 0100 0000 
+    FLAG_NEGATIVE   byte = 1 << 7  // 1000 0000
 )
+
+type AddressingMode byte 
+
+const (
+    Immediate AddressingMode = iota
+    ZeroPage
+    ZeroPageX
+    ZeroPageY
+    Absolute
+    AbsoluteX
+    AbsoluteY
+    Indirect
+    IndirectX
+    IndirectY
+    Accumulator
+    Implied
+    Relative
+    NoneAddressing
+)
+
+const STACK uint16 = 0x0100;
+const STACK_RESET uint8 = 0xFD;
 
 // Emulates the 6502 CPU.
 type CPU struct {
-    Reg_a uint8     // Accumulator
-    Reg_x uint8     // Index Register x
-    Reg_y uint8     // Index Register y
-    SP uint8        // Stack pointer.
-    Status uint8    // Flags for program.
+    Reg_a byte      // Accumulator
+    Reg_x byte      // Index Register x
+    Reg_y byte      // Index Register y
+    SP byte         // Stack pointer.
+    Status byte     // Flags for program.
     PC uint16       // Program Counter
     Mem *Memory     // The RAM :O
 }
 
 func MakeCPU() *CPU {
-    cpu := CPU{Reg_a: 0, Reg_x: 0, Reg_y: 0, SP: 0, Status: 0, PC: 0, Mem: MakeMemory(65536)};
+    cpu := CPU{Reg_a: 0, Reg_x: 0, Reg_y: 0, SP: STACK_RESET, Status: 0, PC: 0, Mem: MakeMemory(65536)};
     return &cpu;
 }
 
+// Fetches an instruction and executes them in a single tick.
+// Note that this is not a Pipelined implementation.
 func (this *CPU) Run() {
     for {
-        var op_code uint8 = this.Mem.read(this.PC);
+        var code byte = this.Mem.read(this.PC);
+        var instr instruction = Instructions[code]
         this.PC++;
-        
-        switch op_code {
-        case 0xA9:  // LDA #$ff
-            var val uint8 = this.Mem.read(this.PC);
-            this.PC++;
-            this.lda(val);
-        case 0xAA:  // TAX
-            this.tax();    
-        case 0xE8:  // INX
+        switch instr.name {
+        case "ADC":
+        case "AND":
+            this.and(instr.mode);
+        case "ASL":
+            this.asl(instr.mode);
+        case "BCC":
+            this.bcc();
+        case "BCS":
+            this.bcs();
+        case "BEQ":
+            this.beq();
+        case "BIT":
+            this.bit(instr.mode);
+        case "BMI":
+            this.bmi();
+        case "BNE":
+            this.bne();
+        case "BPL":
+            this.bpl();
+        case "BRK":
+            return
+        case "BVC":
+            this.bvc();
+        case "BVS":
+            this.bvs();
+        case "CLC":
+            this.clc();
+        case "CLD":
+            this.cld();
+        case "CLI":
+            this.cli();
+        case "CLV":
+            this.clv();
+        case "CMP":
+            this.cmp(instr.mode);
+        case "CPX":
+            this.cpx(instr.mode);
+        case "CPY":
+            this.cpy(instr.mode);
+        case "DEC":
+            this.dec(instr.mode);
+        case "DEX":
+            this.dex();
+        case "DEY":
+            this.dey();
+        case "EOR":
+            this.eor(instr.mode);
+        case "INC":
+            this.inc(instr.mode);
+        case "INX":
             this.inx();
-        case 0x00:  // BRK
-            return;
+        case "INY":
+            this.iny();
+        case "JMP":
+            this.jmp(instr.mode);
+        case "JSR":
+            this.jsr();
+        case "LDA":
+            this.lda(instr.mode);
+        case "LDX":
+            this.ldx(instr.mode);
+        case "LDY":
+            this.ldy(instr.mode);
+        case "LSR":
+            this.lsr(instr.mode);
+        case "NOP":
+            break // Do nothing
+        case "ORA":
+            this.ora(instr.mode);
+        case "PHA":
+            this.pha();
+        case "PHP":
+            this.php();
+        case "PLA":
+            this.pla();
+        case "PLP":
+            this.plp();
+        case "ROL":
+            this.ROL(instr.mode);
+        case "ROR":
+            this.ROR(instr.mode);
+        case "RTI":
+            this.RTI();
+        case "RTS":
+            this.RTS();
+        case "SBC":
+            this.SBC(instr.mode)
+        case "SEC":
+            this.sec();
+        case "SED":
+            this.SED();
+        case "SEI":
+            this.SEI();
+        case "STA":
+            this.sta(instr.mode)
+        case "STX":
+            this.stx(instr.mode)
+        case "STY":
+            this.sty(instr.mode)
+        case "TAX":
+            this.TAX()
+        case "TAY":
+            this.TAY()
+        case "TSX":
+            this.TSX()
+        case "TXA":
+            this.TXA()
+        case "TXS":
+            this.TXS()
+        case "TYA":
+            this.TYA()
         }
+        this.PC += uint16(instr.size);
+        break
     }
 }
 
@@ -58,6 +184,7 @@ func (this *CPU) reset() {
     this.Reg_x = 0;
     this.Reg_y = 0;
     this.Status = 0;
+    this.SP = STACK_RESET;
     this.PC = this.Mem.readU16(0xFFFC);
 }
 
@@ -67,6 +194,78 @@ func (this *CPU) Load_and_run(program []byte) {
     this.Run();
 }
 
+/*
+Note for self: Recall in CPSC 213, our assembly has different versions of `ld`.
+For example: (a) `ld $5, r0` (b) `ld r1, r0` ... (n) `ld 4(r1), r0`
+
+This is basically the same thing. In which there are different versions, notice that the only thing
+it really changes is the "address from where we read/store in case of st"
+*/
+func (this *CPU) GetOperandAddress(mode AddressingMode) uint16 {
+    switch mode {
+    case Immediate:
+        return this.PC;
+    case ZeroPage:
+        return uint16(this.Mem.read(this.PC));
+    case Absolute:
+        return this.Mem.readU16(this.PC);
+    case ZeroPageX:
+        pos := this.Mem.read(this.PC);
+        addr := uint16(pos + this.Reg_x);
+        return addr;
+    case ZeroPageY:
+        pos := this.Mem.read(this.PC);
+        addr := uint16(pos + this.Reg_y);
+        return addr;
+    case AbsoluteX:
+        pos := this.Mem.readU16(this.PC);
+        addr := pos + uint16(this.Reg_x);
+        return addr;
+    case AbsoluteY:
+        pos := this.Mem.readU16(this.PC);
+        addr := pos + uint16(this.Reg_y);
+        return addr;
+    case IndirectX:
+        base := this.Mem.read(this.PC);
+        ptr := uint8(base) + this.Reg_x;
+        lo := this.Mem.read(uint16(ptr));
+        hi := this.Mem.read(uint16(ptr) + 1);
+        return (uint16(hi) << 8) | (uint16(lo));
+    case IndirectY:
+        base := this.Mem.read(this.PC);
+        lo := this.Mem.read(uint16(base));
+        hi := this.Mem.read(uint16(base) + 1);
+        deref_base := (uint16(hi) << 8) | (uint16(lo));       
+        return deref_base + uint16(this.Reg_y);
+    case NoneAddressing:
+        this.Scream();
+    }
+    return 0;
+}
+
+func (this *CPU) stack_push(data byte) {
+    this.Mem.write(STACK + uint16(this.SP), data);
+    this.SP = this.SP - 1;
+}
+
+func (this *CPU) stack_pop() byte {
+    this.SP = this.SP + 1;
+    return this.Mem.read(STACK + uint16(this.SP));
+}
+
+func (this *CPU) stack_push_U16(data uint16) {
+    hi := uint8(data >> 8)
+    lo := uint8(data & 0xff)
+    this.stack_push(hi);
+    this.stack_push(lo);
+}
+
+func (this *CPU) stack_pop_U16() uint16 {
+    lo := this.stack_pop();
+    hi := this.stack_pop();
+    return uint16(hi) << 8 | uint16(lo);
+}
+
 func (e *CPU) Scream() {
-    fmt.Println("AAAAAAH, the CPU is burning!!!!");
+    panic("It's over... GGs")
 }
---
+++ internal/cpu/opcodes.go
@@ -0,0 +1,200 @@
+package cpu
+
+// Unsure whether including a function pointer would be good here.
+// Or if having a switch statement in cpu is better.
+type instruction struct {
+    code byte
+    name string
+    size uint8
+    cycles uint8
+    mode AddressingMode
+}
+
+// It would be ideal if we could store this in such a way that the index of the code IS the code.
+var Instructions = map[byte]instruction {
+    0x69: {0x69, "ADC", 2, 2, Immediate, },
+    0x65: {0x65, "ADC", 2, 3, ZeroPage},
+    0x75: {0x75, "ADC", 2, 4, ZeroPageX},
+    0x6D: {0x6D, "ADC", 3, 4, Absolute},
+    0x7D: {0x7D, "ADC", 3, 4, AbsoluteX},
+    0x79: {0x79, "ADC", 3, 4, AbsoluteY},
+    0x61: {0x61, "ADC", 2, 6, IndirectX},
+    0x71: {0x71, "ADC", 2, 5, IndirectY},
+
+    0x29: {0x29, "AND", 2, 2, Immediate},
+    0x25: {0x25, "AND", 2, 3, ZeroPage},
+    0x35: {0x35, "AND", 2, 4, ZeroPageX},
+    0x2D: {0x2D, "AND", 3, 4, Absolute},
+    0x3D: {0x3D, "AND", 3, 4, AbsoluteX},
+    0x39: {0x39, "AND", 3, 4, AbsoluteY},
+    0x21: {0x21, "AND", 2, 6, IndirectX},
+    0x31: {0x31, "AND", 2, 5, IndirectY},
+
+    0x0A: {0x0A, "ASL", 1, 2, Accumulator},
+    0x06: {0x06, "ASL", 2, 5, ZeroPage},
+    0x16: {0x16, "ASL", 2, 6, ZeroPageX},
+    0x0E: {0x0E, "ASL", 3, 6, Absolute},
+    0x1E: {0x1E, "ASL", 3, 7, AbsoluteX},
+
+    0x90: {0x90, "BCC", 2, 2, Relative},
+    0xB0: {0xB0, "BCS", 2, 2, Relative},
+    0xF0: {0xF0, "BEQ", 2, 2, Relative},
+    
+    0x24: {0x24, "BIT", 2, 3, ZeroPage},
+    0x2C: {0x2C, "BIT", 3, 4, Absolute},
+    
+    0x30: {0x30, "BMI", 2, 2, Relative},
+    0xD0: {0xD0, "BNE", 2, 2, Relative},
+    0x10: {0x10, "BPL", 2, 2, Relative},
+    
+    0x00: {0x00, "BRK", 1, 7, Implied},
+    
+    0x50: {0x50, "BVC", 2, 2, Relative},
+    0x70: {0x70, "BVS", 2, 2, Relative},
+    
+    0x18: {0x18, "CLC", 1, 2, Implied},
+    0xD8: {0xD8, "CLD", 1, 2, Implied},
+    0x58: {0x58, "CLI", 1, 2, Implied},
+    0xB8: {0xB8, "CLV", 1, 2, Implied},
+    
+    0xC9: {0xC9, "CMP", 2, 2, Immediate},
+    0xC5: {0xC5, "CMP", 2, 3, ZeroPage},
+    0xD5: {0xD5, "CMP", 2, 4, ZeroPageX},
+    0xCD: {0xCD, "CMP", 3, 4, Absolute},
+    0xDD: {0xDD, "CMP", 3, 4, AbsoluteX},
+    0xD9: {0xD9, "CMP", 3, 4, AbsoluteY},
+    0xC1: {0xC1, "CMP", 2, 6, IndirectX},
+    0xD1: {0xD1, "CMP", 2, 5, IndirectY},
+    
+    0xE0: {0xE0, "CPX", 2, 2, Immediate},
+    0xE4: {0xE4, "CPX", 2, 3, ZeroPage},
+    0xEC: {0xEC, "CPX", 3, 4, Absolute},
+    
+    0xC0: {0xC0, "CPY", 2, 2, Immediate},
+    0xC4: {0xC4, "CPY", 2, 3, ZeroPage},
+    0xCC: {0xCC, "CPY", 3, 4, Absolute},
+    
+    0xC6: {0xC6, "DEC", 2, 5, ZeroPage},
+    0xD6: {0xD6, "DEC", 2, 6, ZeroPageX},
+    0xCE: {0xCE, "DEC", 3, 6, Absolute},
+    0xDE: {0xDE, "DEC", 3, 7, AbsoluteX},
+    
+    0xCA: {0xCA, "DEX", 1, 2, Implied},
+    0x88: {0x88, "DEY", 1, 2, Implied},
+    
+    0x49: {0x49, "EOR", 2, 2, Immediate},
+    0x45: {0x45, "EOR", 2, 3, ZeroPage},
+    0x55: {0x55, "EOR", 2, 4, ZeroPageX},
+    0x4D: {0x4D, "EOR", 3, 4, Absolute},
+    0x5D: {0x5D, "EOR", 3, 4, AbsoluteX},
+    0x59: {0x59, "EOR", 3, 4, AbsoluteY},
+    0x41: {0x41, "EOR", 2, 6, IndirectX},
+    0x51: {0x51, "EOR", 2, 5, IndirectY},
+    
+    0xE6: {0xE6, "INC", 2, 5, ZeroPage},
+    0xF6: {0xF6, "INC", 2, 6, ZeroPageX},
+    0xEE: {0xEE, "INC", 3, 6, Absolute},
+    0xFE: {0xFE, "INC", 3, 7, AbsoluteX},
+    
+    0xE8: {0xE8, "INX", 1, 2, Implied},
+    0xC8: {0xC8, "INY", 1, 2, Implied},
+    
+    0x4C: {0x4C, "JMP", 3, 3, Absolute},
+    0x6C: {0x6C, "JMP", 3, 5, Indirect},
+    
+    0x20: {0x20, "JSR", 3, 6, Absolute},
+    
+    0xA9: {0xA9, "LDA", 2, 2, Immediate},
+    0xA5: {0xA5, "LDA", 2, 3, ZeroPage},
+    0xB5: {0xB5, "LDA", 2, 4, ZeroPageX},
+    0xAD: {0xAD, "LDA", 3, 4, Absolute},
+    0xBD: {0xBD, "LDA", 3, 4, AbsoluteX},
+    0xB9: {0xB9, "LDA", 3, 4, AbsoluteY},
+    0xA1: {0xA1, "LDA", 2, 6, IndirectX},
+    0xB1: {0xB1, "LDA", 2, 5, IndirectY},
+    
+    0xA2: {0xA2, "LDX", 2, 2, Immediate},
+    0xA6: {0xA6, "LDX", 2, 3, ZeroPage},
+    0xB6: {0xB6, "LDX", 2, 4, ZeroPageY},
+    0xAE: {0xAE, "LDX", 3, 4, Absolute},
+    0xBE: {0xBE, "LDX", 3, 4, AbsoluteY},
+    
+    0xA0: {0xA0, "LDY", 2, 2, Immediate},
+    0xA4: {0xA4, "LDY", 2, 3, ZeroPage},
+    0xB4: {0xB4, "LDY", 2, 4, ZeroPageX},
+    0xAC: {0xAC, "LDY", 3, 4, Absolute},
+    0xBC: {0xBC, "LDY", 3, 4, AbsoluteX},
+    
+    0x4A: {0x4A, "LSR", 1, 2, Accumulator},
+    0x46: {0x46, "LSR", 2, 5, ZeroPage},
+    0x56: {0x56, "LSR", 2, 6, ZeroPageX},
+    0x4E: {0x4E, "LSR", 3, 6, Absolute},
+    0x5E: {0x5E, "LSR", 3, 7, AbsoluteX},
+    
+    0xEA: {0xEA, "NOP", 1, 2, Implied},
+    
+    0x09: {0x09, "ORA", 2, 2, Immediate},
+    0x05: {0x05, "ORA", 2, 3, ZeroPage},
+    0x15: {0x15, "ORA", 2, 4, ZeroPageX},
+    0x0D: {0x0D, "ORA", 3, 4, Absolute},
+    0x1D: {0x1D, "ORA", 3, 4, AbsoluteX},
+    0x19: {0x19, "ORA", 3, 4, AbsoluteY},
+    0x01: {0x01, "ORA", 2, 6, IndirectX},
+    0x11: {0x11, "ORA", 2, 5, IndirectY},
+    
+    0x48: {0x48, "PHA", 1, 3, Implied},
+    0x08: {0x08, "PHP", 1, 3, Implied},
+    0x68: {0x68, "PLA", 1, 4, Implied},
+    0x28: {0x28, "PLP", 1, 4, Implied},
+    
+    0x2A: {0x2A, "ROL", 1, 2, Accumulator},
+    0x26: {0x26, "ROL", 2, 5, ZeroPage},
+    0x36: {0x36, "ROL", 2, 6, ZeroPageX},
+    0x2E: {0x2E, "ROL", 3, 6, Absolute},
+    0x3E: {0x3E, "ROL", 3, 7, AbsoluteX},
+    
+    0x6A: {0x6A, "ROR", 1, 2, Accumulator},
+    0x66: {0x66, "ROR", 2, 5, ZeroPage},
+    0x76: {0x76, "ROR", 2, 6, ZeroPageX},
+    0x6E: {0x6E, "ROR", 3, 6, Absolute},
+    0x7E: {0x7E, "ROR", 3, 7, AbsoluteX},
+    
+    0x40: {0x40, "RTI", 1, 6, Implied},
+    0x60: {0x60, "RTS", 1, 6, Implied},
+    
+    0xE9: {0xE9, "SBC", 2, 2, Immediate},
+    0xE5: {0xE5, "SBC", 2, 3, ZeroPage},
+    0xF5: {0xF5, "SBC", 2, 4, ZeroPageX},
+    0xED: {0xED, "SBC", 3, 4, Absolute},
+    0xFD: {0xFD, "SBC", 3, 4, AbsoluteX},
+    0xF9: {0xF9, "SBC", 3, 4, AbsoluteY},
+    0xE1: {0xE1, "SBC", 2, 6, IndirectX},
+    0xF1: {0xF1, "SBC", 2, 5, IndirectY},
+    
+    0x38: {0x38, "SEC", 1, 2, Implied},
+    0xF8: {0xF8, "SED", 1, 2, Implied},
+    0x78: {0x78, "SEI", 1, 2, Implied},
+    
+    0x85: {0x85, "STA", 2, 3, ZeroPage},
+    0x95: {0x95, "STA", 2, 4, ZeroPageX},
+    0x8D: {0x8D, "STA", 3, 4, Absolute},
+    0x9D: {0x9D, "STA", 3, 5, AbsoluteX},
+    0x99: {0x99, "STA", 3, 5, AbsoluteY},
+    0x81: {0x81, "STA", 2, 6, IndirectX},
+    0x91: {0x91, "STA", 2, 6, IndirectY},
+    
+    0x86: {0x86, "STX", 2, 3, ZeroPage},
+    0x96: {0x96, "STX", 2, 4, ZeroPageY},
+    0x8E: {0x8E, "STX", 3, 4, Absolute},
+    
+    0x84: {0x84, "STY", 2, 3, ZeroPage},
+    0x94: {0x94, "STY", 2, 4, ZeroPageX},
+    0x8C: {0x8C, "STY", 3, 4, Absolute},
+    
+    0xAA: {0xAA, "TAX", 1, 2, Implied},
+    0xA8: {0xA8, "TAY", 1, 2, Implied},
+    0xBA: {0xBA, "TSX", 1, 2, Implied},
+    0x8A: {0x8A, "TXA", 1, 2, Implied},
+    0x9A: {0x9A, "TXS", 1, 2, Implied},
+    0x98: {0x98, "TYA", 1, 2, Implied},
+}
